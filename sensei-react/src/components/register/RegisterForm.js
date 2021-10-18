5ece30776b6934030cfb73e5d1d32d9cc44624e4
Matheus Willock <matheuswillock@gmail.com>
2021-10-18 20:35:05 -0300

    register ready

--- sensei-react/src/components/register/RegisterForm.js
+++ sensei-react/src/components/register/RegisterForm.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Formik, Form, useField } from "formik";
+import { Formik, Form, useField, Field} from "formik";
 import * as Yup from 'yup';
 
 import academiaImg from "../../assets/images/academia.svg";
+
+// Import imgs perfil
+import BorutoPerfil from "../../assets/images/Perfil-image/boruto-uzumaki.svg"
+import HinataPerfil from "../../assets/images/Perfil-image/hinata-hyuga.svg";
+import kakashiPerfil from "../../assets/images/Perfil-image/kakashi-hatake.svg";
+import NarutoPerfil from "../../assets/images/Perfil-image/naruto-uzumaki.svg";
+import ObitoPerfil from "../../assets/images/Perfil-image/obito-uchiha.svg";
+import SaradaPerfil from "../../assets/images/Perfil-image/sarada-uchiha.svg";
+import TsunadePerfil from "../../assets/images/Perfil-image/tsunade.svg";
 
 export default function RegisterForm() {
 
@@ -25,6 +34,7 @@ export default function RegisterForm() {
     );
   };
 
+
   return (
     <Formik
       // Observe que temos que inicializar TODOS os campos com valores. Esses
@@ -41,8 +51,12 @@ export default function RegisterForm() {
           data_nascimento_pessoa: '',
           usuario: '',
           password: '',
+          
+          especialidade: '',
+
           avatar: '',
-          especialidade: ''
+
+          ocupacao: '',
         }
       }
 
@@ -51,44 +65,48 @@ export default function RegisterForm() {
         Yup.object({
           nome_pessoa: Yup.string()
             .min(2, 'Nome é Obrigatório')
-            .required('Required')
+            .required('Qual o seu nome?')
           ,
           sobrenome_pessoa: Yup.string()
             .min(2, 'Sobrenome é Obrigatório')
-            .required('Required')
+            .required('Qual o seu sobrenome?')
           ,
           telefone_pessoa: Yup.string()
             .min(10, 'Telefone deve ter 10 ou 11 digitos - 11xxxxxxxx')
             .max(11, 'Telefone deve ter no máximo 11 digitos - 119xxxxxxxx')
+            .required('Informe o seu telefone')
           ,
           email_pessoa: Yup.string()
-            .min(10, 'Informe e-mail Válido')
             .required('Required')
+            .email('Email inválido')
           ,
           github: Yup.string()
             .min(8, 'Link do GitHub - Opcional')
           ,
-          data_nascimento_pessoa: Yup.string()
+          data_nascimento_pessoa: Yup.date()
             .min(10, 'Data deve ser no formato - DD/MM/AAAA')
-            .required('Required')
+            .required('informe a sua data de nascimento')
           ,
           usuario: Yup.string()
             .min(6, 'Nome de usuário deve ter no mínimo 6 digitos')
             .max(8, 'Nome de usuário deve ter no máximo 10 digitos')
-            .required('Required')
+            .required('Informe a senha')
           ,
           password: Yup.string()
             .min(8, 'Senha de usuário deve ter no mínimo 8 digitos')
             .max(16, 'Senha de usuário deve ter no máximo 16 digitos')
-            .required('Required')
-          ,
-          avatar: Yup.string()
-            .required('Required')
-          ,
+            .required('Informe a senha')
+          ,          
           especialidade: Yup.string()
             .max(30, 'Informe sua especialidade com até 30 posições')
             .min(5, 'Informe sua especialidade com no mínimo 5 posições')
-            .required('Required')
+            .required('Diga-nos a sua especialidade')
+          ,
+          avatar: Yup.string()
+            .required('Escolha um Avatar')
+          ,
+          ocupacao: Yup.string()
+            .required('Diga-nos se você é Sensei ou Aluno')
           ,
 
         })
@@ -107,7 +125,7 @@ export default function RegisterForm() {
 
     >
 
-      <Form >
+      <Form  method="post" action="/login" >
 
         <div className="form">
 
@@ -142,7 +160,7 @@ export default function RegisterForm() {
             <InputField
               id="input-field-email_pessoa"
               name="email_pessoa"
-              type="text"
+              type="email"
               placeholder="Informe seu email pessoal"
             />
 
@@ -182,21 +200,74 @@ export default function RegisterForm() {
             />
 
             <InputField
-              id="input-field-avatar"
-              name="avatar"
-              type="comboBox"
-              placeholder="Selecione seu Avatar"
-            />
-
-            <InputField
               id="input-field-especialidade"
               name="especialidade"
               type="text"
-              placeholder="Seu sua especialidade de Sensei"
+              placeholder="Seu/sua especialidade de Sensei"
             />
+
+            <h2 className="radio-h2">Você será ...</h2>
+            <div role="group" aria-labelledby="my-radio-group" className="toggle-radio">
+
+              <label className="radio-label">
+                <Field type="radio" name="avatar" value="sensei" />
+                Sensei
+              </label>
+
+              <label className="radio-label">
+                <Field type="radio" name="avatar" value="aluno" />
+                Aluno
+              </label>
+            </div>
+        
+            <h2 className="radio-h2">Selecione o seu avatar</h2>
+          
+            <div role="group" aria-labelledby="my-radio-group" className="radio-group">
+
+              <label className="radio-label">
+                <Field type="radio" name="avatar" value="../../assets/images/Perfil-image/boruto-uzumaki.svg" />
+                <img className="radio-img" src={BorutoPerfil} />
+              </label>
+
+              <label className="radio-label">
+                <Field type="radio" name="avatar" value="../../assets/images/Perfil-image/hinata-hyuga.svg" />
+                <img className="radio-img" src={HinataPerfil} />
+              </label>
+
+              <label className="radio-label">
+                <Field type="radio" name="avatar" value="../../assets/images/Perfil-image/tsunade.svg" />
+                <img className="radio-img" src={TsunadePerfil} />
+              </label>
+
+              <label className="radio-label">
+                <Field type="radio" name="avatar" value="../../assets/images/Perfil-image/sarada-uchiha.svg" />
+                <img className="radio-img" src={SaradaPerfil} />
+              </label>
+
+              <label className="radio-label">
+                <Field type="radio" name="avatar" value="../../assets/images/Perfil-image/obito-uchiha.svg" />
+                <img className="radio-img" src={ObitoPerfil} />
+              </label>
+
+              <label className="radio-label">
+                <Field type="radio" name="avatar" value="../../assets/images/Perfil-image/naruto-uzumaki.svg" />
+                <img className="radio-img" src={NarutoPerfil} />
+              </label>
+
+              <label className="radio-label">
+                <Field type="radio" name="avatar" value="../../assets/images/Perfil-image/kakashi-hatake.svg" />
+                <img className="radio-img" src={kakashiPerfil} />
+              </label>
+              
+              
+            </div>
 
 
           </div>
+            
+          
+
+            
 
           <button className="btn-submit" type="submit">Cadastrar-se</button>
 
